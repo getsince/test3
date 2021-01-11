cd89e12eae58647e83752a8d8135f9115977c35a
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-01-11 14:37:16 +0300

    WIP

--- lib/t_web/controllers/helpers.ex
+++ lib/t_web/controllers/helpers.ex
@@ -5,9 +5,9 @@ defmodule TWeb.ControllerHelpers do
     (attrs["meta"] || %{})
     |> Map.put("ip", conn.remote_ip |> :inet.ntoa() |> to_string())
     |> Map.put("user-agent", conn |> get_req_header("user-agent") |> List.first())
-    |> maybe_put("code", attrs["code"])
-    |> maybe_put("ref", attrs["ref"])
-    |> maybe_put("click_id", attrs["click_id"])
+    |> maybe_put("code", conn.query_params["code"])
+    |> maybe_put("ref", conn.query_params["ref"])
+    |> maybe_put("click_id", conn.query_params["click_id"])
   end
 
   defp maybe_put(map, _key, nil), do: map

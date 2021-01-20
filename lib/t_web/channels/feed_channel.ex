0e45f82f3e8e2056f4ced54dfbf5a9d91083707f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-01-20 11:42:33 +0300

    timezone based feed

--- lib/t_web/channels/feed_channel.ex
+++ lib/t_web/channels/feed_channel.ex
@@ -4,24 +4,58 @@ defmodule TWeb.FeedChannel do
   alias T.{Feeds, Matches, Accounts}
 
   @impl true
-  def join("feed:" <> user_id, _params, socket) do
+  def join("feed:" <> user_id, %{"timezone" => timezone}, socket) do
     ChannelHelpers.verify_user_id(socket, user_id)
     # ChannelHelpers.ensure_onboarded(socket)
 
-    :ok = Feeds.subscribe(user_id)
-
     # TODO show who's online
-
-    # TODO schedule feed refresh
 
     if match = Matches.get_current_match(user_id) do
       other_profile = Matches.get_other_profile_for_match!(match, user_id)
+      # TODO close channel
       {:ok, %{match: render_match(match, other_profile)}, socket}
     else
+      :ok = Feeds.subscribe(user_id)
+
+      datetime = local_datetime_now(timezone)
       my_profile = Accounts.get_profile!(socket.assigns.current_user)
-      feed = Feeds.get_or_create_feed(my_profile)
-      {:ok, %{feed: render_profiles(feed)}, socket}
+      feed = Feeds.get_or_create_feed(my_profile, DateTime.to_date(datetime))
+      schedule_feed_refresh_at_midnight(datetime, timezone)
+
+      {:ok, %{feed: render_profiles(feed)}, assign(socket, timezone: timezone)}
     end
+  end
+
+  defp local_datetime_now(timezone) do
+    # |> IO.inspect(label: "now")
+    DateTime.shift_zone!(DateTime.utc_now(), timezone)
+  end
+
+  defp mindnight_next_day(date, timezone) do
+    # |> IO.inspect(label: "midnight next day")
+    DateTime.new(date, Time.new!(0, 0, 0), timezone)
+  end
+
+  # TODO extract and test
+  defp schedule_feed_refresh_at_midnight(datetime_now, timezone) do
+    # |> IO.inspect(label: "next_day")
+    next_day = datetime_now |> DateTime.to_date() |> Date.add(1)
+
+    midnight_next_day =
+      case mindnight_next_day(next_day, timezone) do
+        {:ok, datetime} -> datetime
+        {:ambiguous, _first_, second_dt} -> second_dt
+        {:gap, _just_before, just_after} -> just_after
+      end
+
+    # |> IO.inspect(label: "diff")
+    seconds_diff = DateTime.diff(midnight_next_day, datetime_now)
+
+    Process.send_after(
+      self(),
+      {__MODULE__, [:update_feed], next_day},
+      :timer.seconds(seconds_diff)
+    )
   end
 
   defp render_match(match, profile) do
@@ -71,8 +105,6 @@ defmodule TWeb.FeedChannel do
     end
   end
 
-  # TODO fetch feed
-
   @impl true
   def handle_info({Feeds, [:matched], match}, socket) do
     other_profile = Matches.get_other_profile_for_match!(match, socket.assigns.current_user.id)
@@ -84,6 +116,18 @@ defmodule TWeb.FeedChannel do
   def handle_info({Matches, [:pending_match_activated], match}, socket) do
     other_profile = Matches.get_other_profile_for_match!(match, socket.assigns.current_user.id)
     push(socket, "matched", %{match: render_match(match, other_profile)})
+    {:noreply, socket}
+  end
+
+  # TODO test
+  def handle_info({__MODULE__, [:update_feed], date}, socket) do
+    %{timezone: timezone, current_user: user} = socket.assigns
+
+    profile = Accounts.get_profile!(user)
+    feed = Feeds.get_or_create_feed(profile, date)
+    schedule_feed_refresh_at_midnight(local_datetime_now(timezone), timezone)
+    push(socket, "feed:update", %{feed: render_profiles(feed)})
+
     {:noreply, socket}
   end
 end

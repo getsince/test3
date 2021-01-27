2ad604dc8903610b81ea43f0b4f151428dc1fd8d
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-01-28 01:01:19 +0300

    render message urls

--- lib/t_web/views/match_view.ex
+++ lib/t_web/views/match_view.ex
@@ -1,8 +1,17 @@
 defmodule TWeb.MatchView do
   use TWeb, :view
   alias T.Matches.Message
+  alias T.Media
 
   def render("message.json", %{message: %Message{} = message}) do
-    Map.take(message, [:id, :author_id, :timestamp, :kind, :data])
+    message
+    |> Map.take([:id, :author_id, :timestamp, :kind, :data])
+    |> maybe_render_s3_url()
   end
+
+  defp maybe_render_s3_url(%{data: %{"s3_key" => s3_key} = data} = message) do
+    %{message | data: Map.put(data, "url", Media.url(s3_key))}
+  end
+
+  defp maybe_render_s3_url(message), do: message
 end

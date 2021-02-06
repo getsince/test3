4499c501e6c1fe0652f74b70d0d9d56509ac6558
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-02-06 14:07:36 +0300

    add log out apns test

--- lib/t_web/channels/profile_channel.ex
+++ lib/t_web/channels/profile_channel.ex
@@ -39,17 +39,6 @@ defmodule TWeb.ProfileChannel do
     {:reply, {:ok, %{profile: render_profile(profile)}}, assign(socket, profile: profile)}
   end
 
-  # TODO test
-  def handle_in(
-        "save-device-id",
-        %{"device" => %{"id" => device_id, "platform" => "ios"}},
-        socket
-      ) do
-    %{current_user: user, token: token} = socket.assigns
-    :ok = Accounts.save_apns_device_id(user.id, token, device_id)
-    {:reply, :ok, socket}
-  end
-
   def handle_in("submit", %{"profile" => params}, socket) do
     %{profile: profile, current_user: user} = socket.assigns
 

bdab3aa6c964bef9cd629ccfd49efb733fc407d2
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-03-22 12:08:51 +0300

    clean deps

--- lib/t_mailer/email.ex
+++ lib/t_mailer/email.ex
@@ -1,26 +1,26 @@
-defmodule T.Mailer.Email do
-  use Bamboo.Phoenix, view: TWeb.EmailView
-  alias T.Mailer
+# defmodule T.Mailer.Email do
+#   use Bamboo.Phoenix, view: TWeb.EmailView
+#   alias T.Mailer
 
-  def added_to_waitlist_email(receiver_address, opts \\ []) do
-    maybe_set_locale(opts)
+#   def added_to_waitlist_email(receiver_address, opts \\ []) do
+#     maybe_set_locale(opts)
 
-    base_email(receiver_address)
-    # TODO
-    |> subject("waitlist")
-    |> render("added_to_waitlist.text")
-  end
+#     base_email(receiver_address)
+#     # TODO
+#     |> subject("waitlist")
+#     |> render("added_to_waitlist.text")
+#   end
 
-  defp base_email(receiver) do
-    new_email()
-    |> to({"Наш пользователь", receiver})
-    # TODO
-    |> from({"Синсь", Mailer.our_address()})
-  end
+#   defp base_email(receiver) do
+#     new_email()
+#     |> to({"Наш пользователь", receiver})
+#     # TODO
+#     |> from({"Синсь", Mailer.our_address()})
+#   end
 
-  defp maybe_set_locale(opts) do
-    if locale = opts[:locale] do
-      Gettext.put_locale(locale)
-    end
-  end
-end
+#   defp maybe_set_locale(opts) do
+#     if locale = opts[:locale] do
+#       Gettext.put_locale(locale)
+#     end
+#   end
+# end

d1cdd54d63e114732ec4705a6c54a2267cc34c7f
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-05-05 19:09:59 +0300

    new likes channel

--- lib/t/feed/profile_like.ex
+++ lib/t/feed/profile_like.ex
@@ -8,7 +8,10 @@ defmodule T.Feeds.ProfileLike do
   schema "liked_profiles" do
     belongs_to :by_user, User, primary_key: true
     belongs_to :user, User, primary_key: true
+
+    field :liker_profile, :map, virtual: true
     field :seen?, :boolean
+
     timestamps(updated_at: false)
   end
 end

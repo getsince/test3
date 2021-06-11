1b76c04e39b30a2595589e3321a5c47bb38a9143
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-06-11 10:55:17 +0300

    fix cancelling multiple like jobs

--- lib/t/feed/like_job.ex
+++ lib/t/feed/like_job.ex
@@ -1,9 +1,10 @@
 defmodule T.Feeds.LikeJob do
-  use Oban.Worker, unique: [period: 60], queue: :likes
+  use Oban.Worker, queue: :likes, max_attempts: 1
 
   @impl true
   def perform(%Oban.Job{args: args}) do
     %{"by_user_id" => by_user_id, "user_id" => user_id} = args
     T.Feeds.like_profile(by_user_id, user_id)
+    :ok
   end
 end

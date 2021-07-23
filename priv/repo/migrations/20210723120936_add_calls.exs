be7e9ecf1f22dfb186716c62a44afb0e11320834
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-07-23 20:45:16 +0300

    format

--- priv/repo/migrations/20210723120936_add_calls.exs
+++ priv/repo/migrations/20210723120936_add_calls.exs
@@ -5,8 +5,13 @@ defmodule T.Repo.Migrations.AddCalls do
     create table(:calls, primary_key: false) do
       add :id, :uuid, primary_key: true
 
-      add :caller_id, references(:profiles, on_delete: :delete_all, column: :user_id, type: :uuid), null: false
-      add :called_id, references(:profiles, on_delete: :delete_all, column: :user_id, type: :uuid), null: false
+      add :caller_id,
+          references(:profiles, on_delete: :delete_all, column: :user_id, type: :uuid),
+          null: false
+
+      add :called_id,
+          references(:profiles, on_delete: :delete_all, column: :user_id, type: :uuid),
+          null: false
 
       add :ended_at, :timestamptz
 

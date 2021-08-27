da7a7d978f877f9da9d5cafb6df87e3a04a4dea8
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-08-27 22:46:49 +0300

    fix bench config

--- config/bench.exs
+++ config/bench.exs
@@ -1,3 +1,6 @@
 import Config
 
 config :t, T.Accounts.UserNotifier, adapter: T.Accounts.LocalUserNotifier
+config :t, T.Media.Client, adapter: T.Media.S3Client
+config :t, T.Bot, adapter: T.Bot.API
+config :t, T.PushNotifications.APNS, adapter: T.PushNotifications.APNS.Pigeon

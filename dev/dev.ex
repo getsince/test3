eb8991e84e14aae0475a2fbdc34ca98dc5867713
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-10-02 01:36:17 +0300

    eh

--- dev/dev.ex
+++ dev/dev.ex
@@ -81,6 +81,254 @@ defmodule Dev do
   end
 end
 
+defmodule FeedCache do
+  use GenServer
+  require Logger
+
+  def setup do
+    {:ok, _pid} = FeedCache.start_link([])
+    FeedCache.demo_user()
+  end
+
+  def story do
+    [
+      %{
+        "background" => %{"s3_key" => "1e08a6a1-c99a-4ac0-bc75-aef5e03fab8a"},
+        "labels" => [
+          %{
+            "answer" => "Moscow",
+            "position" => [16.39473684210526, 653.8865836791149],
+            "question" => "city",
+            "rotation" => 0,
+            "value" => "Moscow",
+            "zoom" => 1
+          },
+          %{
+            "answer" => "1992-06-15T09:29:34Z",
+            "position" => [17.76315789473683, 711.5131396957124],
+            "question" => "birthdate",
+            "rotation" => 0,
+            "value" => "29",
+            "zoom" => 1
+          },
+          %{
+            "answer" => "marketing",
+            "position" => [17.894736842105278, 768.5200553250346],
+            "question" => "occupation",
+            "rotation" => 0,
+            "value" => "marketing",
+            "zoom" => 1
+          }
+        ],
+        "size" => [414, 896]
+      },
+      %{
+        "background" => %{"s3_key" => "7b2ee4ac-f52f-4428-8da5-7538cab82ca2"},
+        "labels" => [
+          %{
+            "answer" => "dog",
+            "position" => [32.45421914384576, 508.55878284923926],
+            "question" => "pets",
+            "rotation" => 0,
+            "value" => "dog",
+            "zoom" => 1
+          },
+          %{
+            "answer" => "",
+            "position" => [30.007430073955632, 125.42931999400341],
+            "question" => "height",
+            "rotation" => -0.1968919380719123,
+            "value" => "169 cm",
+            "zoom" => 0.929552717754467
+          }
+        ],
+        "size" => [414, 896]
+      },
+      %{
+        "background" => %{"s3_key" => "030cf497-eabc-40e8-9e25-22f0549d5828"},
+        "labels" => [
+          %{
+            "answer" => "politics",
+            "position" => [255.18867086246476, 172.7136929460581],
+            "question" => "interests",
+            "rotation" => 0,
+            "value" => "politics",
+            "zoom" => 1
+          },
+          %{
+            "answer" => "culture and art",
+            "position" => [188.0263157894737, 231.47884213914443],
+            "question" => "books",
+            "rotation" => 0,
+            "value" => "culture and art",
+            "zoom" => 1
+          }
+        ],
+        "size" => [414, 896]
+      },
+      %{
+        "background" => %{"s3_key" => "8ecc0c2c-89fb-437f-bfae-4893cba3c7fc"},
+        "labels" => [
+          %{
+            "answer" => "The Sopranos",
+            "position" => [9.407094262700014, 362.32365145228215],
+            "question" => "tv_shows",
+            "rotation" => 0,
+            "value" => "The Sopranos",
+            "zoom" => 1
+          }
+        ],
+        "size" => [414, 896]
+      }
+    ]
+  end
+
+  def demo_user do
+    story = story() |> :erlang.term_to_binary()
+
+    # binary story
+    # 5 MB for 10_000 -> 100 MB for 200_000 -> 1 GB for 2_000_000
+    # map story
+    # 50 MB for 10_000 -> 100 MB for 20_000 -> 1 GB for 200_000
+    1..10000
+    |> Enum.map(fn _ ->
+      user_id = Ecto.Bigflake.UUID.bingenerate()
+      session_id = Ecto.Bigflake.UUID.bingenerate()
+      {user_id, session_id, %{gender: "M", preferences: ["F"], name: "Ruslan", story: story}}
+    end)
+    |> FeedCache.put_many_users()
+  end
+
+  # def compress_story([%{"background" => %"s3_key" => key} | rest]) do
+  #   s3_key = Ecto.UUID.dump!(s3_key)
+
+  # end
+
+  # def compress_story([]) do
+  #   <<>>
+  # end
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @session2profiles :session2profiles
+  @profiles :profiles
+
+  @impl true
+  def init(_opts) do
+    for my_gender <- ["M", "F"], want_gender <- ["M", "F"] do
+      :ets.new(table(my_gender, want_gender), [:named_table, :ordered_set])
+    end
+
+    # TODO compare perf with duplicate_bag (~10% faster)
+    :ets.new(@session2profiles, [:named_table])
+    :ets.new(@profiles, [:named_table])
+
+    {:ok, nil}
+  end
+
+  def table(my_gender, want_gender)
+  def table("M", "F"), do: :active_FM
+  def table("F", "M"), do: :active_MF
+  def table("M", "M"), do: :active_MM
+  def table("F", "F"), do: :active_FF
+
+  @spec fetch_feed(binary, String.t(), [String.t()], pos_integer()) ::
+          {binary, [{binary, String.t(), String.t(), [map]}]}
+  def fetch_feed(cursor \\ nil, gender, preferences, limit \\ 10)
+
+  # with cursor and single gender preference
+  def fetch_feed(<<_::192>> = cursor, gender, [preference], limit) do
+    do_fetch_feed(table(gender, preference), cursor, limit, _acc = [])
+  end
+
+  def fetch_feed(nil = _cursor, gender, [preference], limit) do
+    do_fetch_feed(table(gender, preference), _cursor = <<0::192>>, limit, _acc = [])
+  end
+
+  defp do_fetch_feed(tab, cursor, limit, acc) when limit > 0 do
+    case :ets.next(tab, cursor) do
+      <<_geohash::64, session_id::16-bytes>> = cursor ->
+        do_fetch_feed(tab, cursor, limit - 1, [fetch_feed_profile(session_id) | acc])
+
+      :"$end_of_table" ->
+        {cursor, :lists.reverse(acc)}
+    end
+  end
+
+  defp do_fetch_feed(_tab, cursor, 0, acc) do
+    {cursor, :lists.reverse(acc)}
+  end
+
+  def fetch_feed_profile(<<_::128>> = session_id) do
+    [{^session_id, user_id}] = :ets.lookup(@session2profiles, session_id)
+    [{^user_id, _name, _gender, _story} = profile] = :ets.lookup(@profiles, user_id)
+    profile
+  end
+
+  def fetch_feed_profile(<<_::288>> = session_id) do
+    session_id
+    |> Ecto.UUID.dump!()
+    |> fetch_feed_profile()
+  end
+
+  def put_user(<<_::128>> = user_id, <<_::128>> = session_id, data) do
+    GenServer.call(__MODULE__, {:put, user_id, session_id, data})
+  end
+
+  def put_many_users(users) do
+    GenServer.call(__MODULE__, {:put, users})
+  end
+
+  def remove_session(session_id) do
+    GenServer.call(__MODULE__, {:remove, session_id})
+  end
+
+  def stats do
+    %{
+      profiles: :ets.info(@profiles)
+    }
+  end
+
+  @impl true
+  def handle_call({:put, <<_::128>> = user_id, <<_::128>> = session_id, data}, _from, state) do
+    insert_user(user_id, session_id, data)
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:put, users}, _from, state) when is_list(users) do
+    Enum.each(users, fn {user_id, session_id, data} -> insert_user(user_id, session_id, data) end)
+    {:reply, :ok, state}
+  end
+
+  def handle_call({:remove, <<_::128>> = session_id}, _from, state) do
+    # TODO improve, only delete from tables that have the user, need to know gender preferences
+    for g1 <- ["M", "F"],
+        g2 <- ["M", "F"],
+        do: :ets.delete(table(g1, g2), <<0::64, session_id::bytes>>)
+
+    [{^session_id, user_id}] = :ets.lookup(@session2profiles, session_id)
+    :ets.delete(@session2profiles, session_id)
+    :ets.delete(@profiles, user_id)
+
+    {:reply, :ok, state}
+  end
+
+  def handle_call(message, _from, state) do
+    Logger.error("unhandled message in FeedCache: " <> inspect(message))
+    {:reply, {:error, :badarg}, state}
+  end
+
+  defp insert_user(<<_::128>> = user_id, <<_::128>> = session_id, data) do
+    %{gender: gender, preferences: prefs, name: name, story: story} = data
+
+    :ets.insert(@profiles, {user_id, name, gender, story})
+    :ets.insert(@session2profiles, {session_id, user_id})
+    for pref <- prefs, do: :ets.insert(table(pref, gender), {<<0::64, session_id::bytes>>})
+  end
+end
+
 # defmodule ActiveSessionCache do
 #   @moduledoc false
 #   use GenServer

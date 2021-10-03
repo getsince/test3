2108f836b016aa8f7d706d23161e6412ea150f83
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-10-04 01:16:01 +0300

    eh

--- test/t/feeds/feed_cache_test.exs
+++ test/t/feeds/feed_cache_test.exs
@@ -8,28 +8,71 @@ defmodule T.Feeds.FeedCacheTest do
   end
 
   test "empty tables" do
-    assert {[active_FF: <<0::128>>], []} =
-             FeedCache.fetch_feed(_my_gender = "F", _my_preference = ["F"])
+    no_filter = MapSet.new()
 
-    assert {[active_MF: <<0::128>>, active_FF: <<0::128>>], []} =
-             FeedCache.fetch_feed(_my_gender = "F", _my_preference = ["F", "M"])
+    assert {<<_cursor::18-bytes>>, []} =
+             FeedCache.feed_init(_my_gender = "F", _my_preference = ["F"], _limit = 10, no_filter)
 
-    assert {[active_NF: <<0::128>>, active_MF: <<0::128>>, active_FF: <<0::128>>], []} =
-             FeedCache.fetch_feed(_my_gender = "F", _my_preference = ["F", "M", "N"])
+    assert {<<_cursor::36-bytes>>, []} =
+             FeedCache.feed_init(
+               _my_gender = "F",
+               _my_preference = ["F", "M"],
+               _limit = 10,
+               no_filter
+             )
+
+    assert {<<_cursor::54-bytes>>, []} =
+             FeedCache.feed_init(
+               _my_gender = "F",
+               _my_preference = ["F", "M", "N"],
+               _limit = 10,
+               no_filter
+             )
   end
 
   test "with ending tables" do
+    %{user_id: user_id, session_id: session_id} = fake_user()
+    no_filter = MapSet.new()
+
+    assert {cursor, feed} =
+             FeedCache.feed_init(
+               _my_gender = "F",
+               _my_preference = ["M"],
+               _limit = 10,
+               no_filter
+             )
+
+    assert cursor == "MF" <> session_id
+    assert feed == [{user_id, "John", "M", [%{"some" => "story"}]}]
+    assert {^cursor, []} = FeedCache.feed_cont(cursor, _count = 10, no_filter)
+  end
+
+  test "filter" do
+    %{user_id: user_id, session_id: session_id} = fake_user()
+
+    assert {cursor, []} =
+             FeedCache.feed_init(
+               _my_gender = "F",
+               _my_preference = ["M"],
+               _limit = 10,
+               _filter = MapSet.new([user_id])
+             )
+
+    assert cursor == "MF" <> session_id
+  end
+
+  defp fake_user(opts \\ []) do
     user_id = Ecto.Bigflake.UUID.bingenerate()
     session_id = Ecto.Bigflake.UUID.bingenerate()
-    data = %{gender: "M", preferences: ["F"], name: "John", story: [%{"some" => "story"}]}
+
+    data = %{
+      gender: opts[:gender] || "M",
+      preferences: opts[:preferences] || ["F"],
+      name: opts[:name] || "John",
+      story: opts[:story] || [%{"some" => "story"}]
+    }
 
     :ok = FeedCache.put_user(user_id, session_id, data)
-
-    assert {cursor, feed} = FeedCache.fetch_feed(_my_gender = "F", _my_preference = ["M"])
-    assert cursor == [active_MF: session_id]
-    assert feed == [{user_id, "John", "M", [%{"some" => "story"}]}]
-
-    assert {cursor, []} = FeedCache.fetch_feed(session_id, "F", ["M"])
-    assert cursor == [active_MF: session_id]
+    %{user_id: user_id, session_id: session_id}
   end
 end

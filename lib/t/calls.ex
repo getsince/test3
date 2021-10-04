c07d98f19e843ddd8f8fa0aa608682ecf151e0fb
Vladislav <orlovvlad95.95@gmail.com>
2021-10-04 22:43:16 +0300

    call's notification fix

--- lib/t/calls.ex
+++ lib/t/calls.ex
@@ -108,6 +108,7 @@ defmodule T.Calls do
   @spec end_call(Ecto.UUID.t(), Ecto.UUID.t(), DateTime.t()) :: :ok
   def end_call(user_id, call_id, now \\ utc_now()) do
     m = "user #{user_id} ended a call #{call_id}"
+    
     Logger.warn(m)
     Bot.async_post_message(m)
     {1, _} =

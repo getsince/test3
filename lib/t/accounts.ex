a7499249c77dbad82630c76640724e0d164f0c6a
Vladislav <orlovvlad95.95@gmail.com>
2021-10-06 14:01:25 +0300

    name from profile

--- lib/t/accounts.ex
+++ lib/t/accounts.ex
@@ -460,16 +460,9 @@ defmodule T.Accounts do
       {:ok, count >= 1}
     end)
     |> Repo.transaction()
-
-    new_user_name = Profile
-    |> where(user_id: ^user_id)
-    |> select([p], p.name)
-    |> Repo.one!()
-    {:ok, new_user_name}
-
     |> case do
       {:ok, %{user: user, profile: %Profile{} = profile}} ->
-        m = "user onboarded #{user.id}"
+        m = "user registried #{user.id}, user name #{profile.name}"
         Logger.warn(m)
         Bot.async_post_message(m)
         {:ok, %Profile{profile | hidden?: false}}

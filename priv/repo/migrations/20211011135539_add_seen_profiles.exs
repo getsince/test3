f411bb6f2bede82b82b92d6d346dc66ba11f420c
akhrail1996 <akhrail1996@gmail.com>
2021-10-12 13:13:43 +0300

    fix format

--- priv/repo/migrations/20211011135539_add_seen_profiles.exs
+++ priv/repo/migrations/20211011135539_add_seen_profiles.exs
@@ -5,8 +5,8 @@ defmodule T.Repo.Migrations.AddSeenProfiles do
 
   def change do
     create table(:seen_profiles, primary_key: false) do
-      add :by_user_id, references(:users, @opts), primary_key: true,  null: false
-      add :user_id, references(:users, @opts), primary_key: true,  null: false
+      add :by_user_id, references(:users, @opts), primary_key: true, null: false
+      add :user_id, references(:users, @opts), primary_key: true, null: false
       timestamps(updated_at: false)
     end
 

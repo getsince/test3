8857ded3863309a81907ef623ba685ab45c636fe
akhrail1996 <akhrail1996@gmail.com>
2021-10-23 14:09:02 +0800

    remove wrong stuff

--- lib/t/feeds.ex
+++ lib/t/feeds.ex
@@ -97,96 +97,22 @@ defmodule T.Feeds do
         ) ::
           {[feed_profile], feed_cursor}
   def fetch_feed(user_id, location, gender, gender_preferences, count, feed_cursor) do
-    case feed_cursor do
-      nil -> save_eligible_feed_profiles_to_memory(user_id, location, gender, gender_preferences)
-      _ -> :ok
-    end
-
-    feed_profiles_with_cursor = feed_profiles_from_memory(user_id, feed_cursor, count)
+    feed_profiles =
+      feed_profiles_q(user_id, gender, gender_preferences, feed_cursor)
+      |> limit(^count)
+      |> select([p], {p, distance_km(^location, p.location)})
+      |> Repo.all()
 
     feed_cursor =
-      if last = List.last(feed_profiles_with_cursor) do
-        {cursor, _feed_profile} = last
-        cursor
+      if last = List.last(feed_profiles) do
+        {feed_profile, _distance} = last
+        feed_profile.last_active
       else
         feed_cursor
       end
 
-    feed_profiles =
-      feed_profiles_with_cursor
-      |> Enum.map(fn {_cursor, profile} ->
-        profile
-      end)
-
     {feed_profiles, feed_cursor}
   end
-
-  def save_eligible_feed_profiles_to_memory(user_id, location, gender, gender_preferences) do
-    table = String.to_atom("feed_#{user_id}")
-
-    case :ets.whereis(table) do
-      :undefined -> :ok
-      _ -> :ets.delete(table)
-    end
-
-    :ets.new(table, [:ordered_set, :named_table, read_concurrency: true])
-
-    eligible_feed_profiles =
-      feed_profiles_q(user_id, gender, gender_preferences)
-      |> select([p], {p, distance_km(^location, p.location)})
-      |> Repo.all()
-
-    most_liked_feed_profiles =
-      Enum.sort_by(eligible_feed_profiles, fn {p, _} -> p.times_liked end, :desc)
-
-    recently_active_feed_profiles =
-      Enum.sort_by(eligible_feed_profiles, fn {p, _} -> p.last_active end, {:desc, DateTime})
-
-    feed_profiles = mix_feed_profiles(most_liked_feed_profiles, recently_active_feed_profiles)
-
-    for feed_profile <- feed_profiles do
-      id = Ecto.Bigflake.UUID.generate()
-      :ets.insert(table, {id, feed_profile})
-    end
-  end
-
-  defp mix_feed_profiles(set1, set2) do
-    feed_profiles =
-      Enum.zip(set1, set2)
-      |> Enum.flat_map(fn {p1, p2} -> [p1, p2] end)
-
-    Enum.uniq(feed_profiles)
-  end
-
-  def feed_profiles_from_memory(user_id, feed_cursor, count) do
-    table_name = String.to_atom("feed_#{user_id}")
-
-    keys = next(table_name, feed_cursor, count)
-
-    feed_profiles =
-      keys
-      |> Enum.map(fn key ->
-        :ets.lookup(table_name, key) |> Enum.at(0)
-      end)
-
-    feed_profiles
-  end
-
-  def next(table, nil, count) when count > 0 do
-    case :ets.first(table) do
-      :"$end_of_table" -> []
-      id -> [id | next(table, id, count - 1)]
-    end
-  end
-
-  def next(table, cursor, count) when count > 0 do
-    case :ets.next(table, cursor) do
-      :"$end_of_table" -> []
-      id -> [id | next(table, id, count - 1)]
-    end
-  end
-
-  def next(_table, _after_id, 0), do: []
 
   @spec get_mate_feed_profile(Ecto.UUID.t()) :: %FeedProfile{} | nil
   def get_mate_feed_profile(user_id) do
@@ -195,12 +121,18 @@ defmodule T.Feeds do
     |> Repo.one()
   end
 
-  defp feed_profiles_q(user_id, gender, gender_preference) do
+  defp feed_profiles_q(user_id, gender, gender_preference, nil) do
     treshold_date = DateTime.utc_now() |> DateTime.add(-30 * 24 * 60 * 60, :second)
 
     filtered_profiles_q(user_id, gender, gender_preference)
     |> where([p], p.user_id != ^user_id)
     |> where([p], p.last_active > ^treshold_date)
+    |> order_by(desc: :last_active)
+  end
+
+  defp feed_profiles_q(user_id, gender, gender_preference, last_date) do
+    feed_profiles_q(user_id, gender, gender_preference, nil)
+    |> where([p], p.last_active < ^last_date)
   end
 
   defp reported_user_ids_q(user_id) do
@@ -279,22 +211,5 @@ defmodule T.Feeds do
     SeenProfile
     |> where([s], s.inserted_at < fragment("now() - ? * interval '1 day'", ^ttl_days))
     |> Repo.delete_all()
-  end
-
-  # TODO
-  # TO BE USED ONCE (THEN DELETED)
-  def count_likes() do
-    FeedProfile
-    |> Repo.update_all(set: [times_liked: 0])
-
-    all_likes = Like |> Repo.all()
-
-    for like <- all_likes do
-      user_id = like.user_id
-
-      FeedProfile
-      |> where(user_id: ^user_id)
-      |> Repo.update_all(inc: [times_liked: 1])
-    end
   end
 end

c0e4cf93de01ba9d383e34a60985ce16dbf2cd69
Vladislav <orlovvlad95.95@gmail.com>
2021-10-24 21:06:15 +0300

    fix migration

--- priv/repo/migrations/20211023090119_add_feeded_profiles.exs
+++ priv/repo/migrations/20211023090119_add_feeded_profiles.exs
@@ -1,14 +1,14 @@
-defmodule T.Repo.Migrations.AddTimesLikedToFeedProfiles do
+defmodule T.Repo.Migrations.AddFeededProfiles do
   use Ecto.Migration
 
-    @opts [on_delete: :delete_all, type: :uuid]
+  @opts [on_delete: :delete_all, type: :uuid]
 
-    def change do
-      create table(:feeded_profiles, primary_key: false) do
-        add :for_user_id, references(:users, @opts), primary_key: true, null: false
-        add :user_id, references(:users, @opts), primary_key: true, null: false
-      end
-
-      create index(:feeded_profiles, [:for_user_id, :user_id])
+  def change do
+    create table(:feeded_profiles, primary_key: false) do
+      add :for_user_id, references(:users, @opts), primary_key: true, null: false
+      add :user_id, references(:users, @opts), primary_key: true, null: false
     end
+
+    create index(:feeded_profiles, [:for_user_id, :user_id])
   end
+end

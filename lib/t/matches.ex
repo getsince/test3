a4d998dda17eb2184d08e074321f563791a8c52c
akhrail1996 <akhrail1996@gmail.com>
2021-11-02 23:53:37 +0800

    todo add test

--- lib/t/matches.ex
+++ lib/t/matches.ex
@@ -428,36 +428,38 @@ defmodule T.Matches do
 
     broadcast_for_user(mate, {__MODULE__, [:timeslot, :accepted], timeslot})
 
-    if DateTime.utc_now() > slot do
-      now_push =
-        DispatchJob.new(%{
-          "type" => "timeslot_accepted_now",
-          "match_id" => match_id,
-          "receiver_id" => mate
-        })
+    case DateTime.compare(slot, reference) do
+      :lt ->
+        now_push =
+          DispatchJob.new(%{
+            "type" => "timeslot_accepted_now",
+            "match_id" => match_id,
+            "receiver_id" => mate
+          })
 
-      Oban.insert(now_push)
-    else
-      accepted_push =
-        DispatchJob.new(%{
-          "type" => "timeslot_accepted",
-          "match_id" => match_id,
-          "receiver_id" => mate
-        })
+        Oban.insert(now_push)
 
-      reminder_push =
-        DispatchJob.new(
-          %{"type" => "timeslot_reminder", "match_id" => match_id, "slot" => slot},
-          scheduled_at: DateTime.add(slot, -15 * 60, :second)
-        )
+      _ ->
+        accepted_push =
+          DispatchJob.new(%{
+            "type" => "timeslot_accepted",
+            "match_id" => match_id,
+            "receiver_id" => mate
+          })
 
-      started_push =
-        DispatchJob.new(
-          %{"type" => "timeslot_started", "match_id" => match_id, "slot" => slot},
-          scheduled_at: slot
-        )
+        reminder_push =
+          DispatchJob.new(
+            %{"type" => "timeslot_reminder", "match_id" => match_id, "slot" => slot},
+            scheduled_at: DateTime.add(slot, -15 * 60, :second)
+          )
 
-      Oban.insert_all([accepted_push, reminder_push, started_push])
+        started_push =
+          DispatchJob.new(
+            %{"type" => "timeslot_started", "match_id" => match_id, "slot" => slot},
+            scheduled_at: slot
+          )
+
+        Oban.insert_all([accepted_push, reminder_push, started_push])
     end
 
     timeslot
--- test/t_web/channels/feed_channel_test.exs
+++ test/t_web/channels/feed_channel_test.exs
@@ -690,7 +690,9 @@ defmodule TWeb.FeedChannelTest do
         # 15:30
         DateTime.new!(date, Time.new!(next_hour, 30, 0)),
         # 15:45
-        DateTime.new!(date, Time.new!(next_hour, 45, 0))
+        DateTime.new!(date, Time.new!(next_hour, 45, 0)),
+        # now
+        DateTime.utc_now() |> DateTime.truncate(:second)
       ]
 
       iso_slots = Enum.map(slots, &DateTime.to_iso8601/1)
@@ -705,7 +707,12 @@ defmodule TWeb.FeedChannelTest do
       {:ok, slots: slots}
     end
 
-    test "with match_id", %{slots: [_s1, s2, _s3] = slots, match: match, socket: socket, me: me} do
+    test "with match_id", %{
+      slots: [_s1, s2, _s3, _s4] = slots,
+      match: match,
+      socket: socket,
+      me: me
+    } do
       iso_slot = DateTime.to_iso8601(s2)
       ref = push(socket, "pick-slot", %{"match_id" => match.id, "slot" => iso_slot})
       assert_reply(ref, :ok, _reply)
@@ -721,7 +728,7 @@ defmodule TWeb.FeedChannelTest do
     end
 
     test "with user_id", %{
-      slots: [_s1, s2, _s3] = slots,
+      slots: [_s1, s2, _s3, _s4] = slots,
       match: match,
       mate: mate,
       socket: socket,
@@ -741,7 +748,28 @@ defmodule TWeb.FeedChannelTest do
       assert push == %{"match_id" => match.id, "selected_slot" => s2}
     end
 
-    test "repick", %{slots: [s1, s2, _s3] = slots, match: match, socket: socket, me: me} do
+    test "now slot accepted", %{
+      slots: [_s1, _s2, _s3, s4] = slots,
+      match: match,
+      socket: socket,
+      me: me
+    } do
+      import Ecto.Query
+
+      iso_slot = DateTime.to_iso8601(s4)
+      ref = push(socket, "pick-slot", %{"match_id" => match.id, "slot" => iso_slot})
+      assert_reply(ref, :ok, _reply)
+
+      assert %Timeslot{} = timeslot = Repo.get_by(Timeslot, match_id: match.id)
+      assert timeslot.picker_id == me.id
+      assert timeslot.slots == slots
+      assert timeslot.selected_slot == s4
+
+      # mate gets a accepted_now push
+      # TODO
+    end
+
+    test "repick", %{slots: [s1, s2, _s3, _s4] = slots, match: match, socket: socket, me: me} do
       iso_slot = DateTime.to_iso8601(s2)
       ref = push(socket, "pick-slot", %{"match_id" => match.id, "slot" => iso_slot})
       assert_reply(ref, :ok, _reply)

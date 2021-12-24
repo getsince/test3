0a47356ad2f7ed11dbc1b09c635a394334095d47
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2021-12-24 15:12:41 +0300

    downgrade finch to 0.9.1

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule T.MixProject do
       {:imgproxy, "~> 2.0"},
       {:rexbug, "~> 1.0"},
       {:geo_postgis, "~> 3.4"},
-      {:finch, "~> 0.8"},
+      {:finch, "~> 0.9.0"},
       {:locus, "~> 2.2"},
       # {:prom_ex, "~> 1.3"},
 

3f4e48a3cce734c36b203073933dd6d45ff0fd8d
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-01-11 18:31:25 +0300

    mark all amtches as having had voicemail

--- priv/repo/migrations/20220101121429_add_match_exchanged_voicemail.exs
+++ priv/repo/migrations/20220101121429_add_match_exchanged_voicemail.exs
@@ -1,9 +1,15 @@
 defmodule T.Repo.Migrations.AddMatchExchangedVoicemail do
   use Ecto.Migration
+  alias T.Repo
 
   def change do
     alter table(:matches) do
       add :exchanged_voicemail, :boolean, default: false, null: false
     end
+
+    flush()
+
+    # to stop matches in between two and seven days from expiring
+    Repo.update_all("matches", set: [exchanged_voicemail: true])
   end
 end

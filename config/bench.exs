eaaf9492a40bb4174a24c706b38d0c53184531f7
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-01-15 14:01:39 +0300

    fix benches

--- config/bench.exs
+++ config/bench.exs
@@ -4,3 +4,4 @@ config :t, T.Media.Client, adapter: T.Media.S3Client
 config :t, T.Bot, adapter: T.Bot.API
 config :t, T.PushNotifications.APNS, adapter: T.PushNotifications.APNS.FinchAdapter
 config :t, T.Twilio, adapter: T.Twilio.HTTP
+config :t, T.Accounts.AppleSignIn, adapter: T.Accounts.AppleSignIn.HTTPAdapter

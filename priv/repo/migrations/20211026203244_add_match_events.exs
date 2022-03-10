84892fcac0be76dd511781551fb4d33e12b8901a
akhrail1996 <akhrail1996@gmail.com>
2022-03-10 16:42:58 -0300

    fix of fixes?

---
+++ priv/repo/migrations/20211026203244_add_match_events.exs
@@ -0,0 +1,42 @@
+defmodule T.Repo.Migrations.AddMatchEvents do
+  use Ecto.Migration
+
+  import Ecto.Query
+
+  def change do
+    create table(:match_events, primary_key: false) do
+      add(:timestamp, :utc_datetime, null: false)
+      add(:match_id, :uuid, null: false)
+      add(:event, :string, null: false)
+    end
+
+    create(index(:match_events, [:match_id, "timestamp desc"]))
+    create(index(:match_events, ["event"], where: "event = 'call_start'"))
+
+    flush()
+
+    match_created_events =
+      "matches"
+      |> select([m], {m.id, m.inserted_at})
+      |> T.Repo.all()
+      |> Enum.map(fn {id, inserted_at} ->
+        %{
+          timestamp: DateTime.from_naive!(inserted_at, "Etc/UTC"),
+          match_id: id,
+          event: "created"
+        }
+      end)
+
+    T.Repo.insert_all("match_events", match_created_events)
+
+    match_created_events =
+      "matches"
+      |> select([m], {m.id})
+      |> T.Repo.all()
+      |> Enum.map(fn {id} ->
+        %{timestamp: DateTime.utc_now(), match_id: id, event: "keepalive"}
+      end)
+
+    T.Repo.insert_all("match_events", match_created_events)
+  end
+end

0d29331d8a4e94c8ac3faae2ca750707752ae011
akhrail1996 <akhrail1996@gmail.com>
2022-03-21 18:42:30 -0300

    process location from socket params and save it to db

--- test/t_web/channels/user_socket_test.exs
+++ test/t_web/channels/user_socket_test.exs
@@ -64,6 +64,28 @@ defmodule TWeb.UserSocketTest do
       assert :error ==
                connect(UserSocket, %{"token" => Accounts.UserToken.encoded_token(token)}, %{})
     end
+
+    test "with location" do
+      user = onboarded_user()
+
+      token =
+        user
+        |> Accounts.generate_user_session_token("mobile")
+        |> Accounts.UserToken.encoded_token()
+
+      assert {:ok, socket} =
+               connect(
+                 UserSocket,
+                 %{"token" => token, "version" => "6.0.1", "location" => [55.755516, 37.615040]},
+                 %{}
+               )
+
+      assert socket.assigns.location == %Geo.Point{coordinates: {37.61504, 55.755516}, srid: 4326}
+
+      profile = Accounts.Profile |> where(user_id: ^user.id) |> Repo.one()
+
+      assert profile.location == %Geo.Point{coordinates: {37.61504, 55.755516}, srid: 4326}
+    end
   end
 
   test "disconnected on log out", %{user: user} do

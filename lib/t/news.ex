225acee9c08a9d9f01a4ca5c67d9b5a69c43272d
akhrail1996 <akhrail1996@gmail.com>
2022-04-09 16:20:33 -0300

    add story editor news, remove older news (#690)
    * add story editor news, remove older news
    
    * fix story order for en news
    
    * fixes

--- lib/t/news.ex
+++ lib/t/news.ex
@@ -9,7 +9,7 @@ defmodule T.News do
   import T.Cluster, only: [primary_rpc: 3]
 
   defp news do
-    [
+    pivot_news = [
       %{
         id: 1,
         timestamp: ~U[2022-03-02 20:31:00Z],
@@ -136,108 +136,327 @@ defmodule T.News do
             "size" => [375, 667]
           }
         ]
-      },
-      %{
-        id: 2,
-        timestamp: ~U[2022-03-23 08:16:00Z],
-        version: "6.1.0",
-        story: [
-          %{
-            "background" => %{"color" => "#111010"},
-            "labels" => [
-              %{
-                "value" => dgettext("news", "Привет! 👋"),
-                "position" => [24.0, 80.0],
-                "background_fill" => "#F97EB9"
-              },
-              %{
-                "value" => dgettext("news", "Мы добавили\nавтоматическую локацию."),
-                "position" => [24.0, 148.0],
-                "background_fill" => "#F97EB9"
-              },
-              %{
-                "value" =>
-                  dgettext(
-                    "news",
-                    "Теперь в ленте показывается\nпримерное расстояние\nдо пользователя."
-                  ),
-                "position" => [24.0, 238.0],
-                "background_fill" => "#F97EB9"
-              },
-              %{
-                "value" =>
-                  dgettext(
-                    "news",
-                    "Ты можешь включить\nавтоматическое определение\nлокации, чтобы она\nоставалась актуальной 👇"
-                  ),
-                "position" => [24.0, 356.0],
-                "background_fill" => "#F97EB9"
-              },
-              %{
-                "action" => "enable_auto_location",
-                "value" => dgettext("news", "Включить авто-локацию"),
-                "position" => [75.0, 502.0],
-                "background_fill" => "#F97EB9"
-              }
-            ],
-            "size" => [375, 667]
-          }
-        ]
-      },
-      %{
-        id: 3,
-        timestamp: ~U[2022-03-31 10:00:00Z],
-        version: "6.1.1",
-        story: [
-          %{
-            "background" => %{"color" => "#111010"},
-            "labels" => [
-              %{
-                "value" => dgettext("news", "Привет! 👋"),
-                "position" => [24.0, 80.0],
-                "background_fill" => "#F97EB9"
-              },
-              %{
-                "value" => dgettext("news", "Мы добавили\nновые контакты:"),
-                "position" => [24.0, 148.0],
-                "background_fill" => "#F97EB9"
-              },
-              %{
-                "position" => [24.0, 238.0],
-                "answer" => "getsinceapp",
-                "question" => "messenger"
-              },
-              %{
-                "position" => [24.0, 306.0],
-                "answer" => "kindly@getsince.app",
-                "question" => "imessage"
-              },
-              %{
-                "position" => [24.0, 374.0],
-                "answer" => "since_app",
-                "question" => "twitter"
-              },
-              %{
-                "position" => [24.0, 442.0],
-                "answer" => "getsince",
-                "question" => "snapchat"
-              },
-              %{
-                "position" => [24.0, 510.0],
-                "answer" => "+541176148981",
-                "question" => "signal"
-              },
-              %{
-                "value" => dgettext("news", "Общайтесь там, где удобно ✌️"),
-                "position" => [24.0, 578.0],
-                "background_fill" => "#F97EB9"
-              }
-            ],
-            "size" => [375, 667]
-          }
-        ]
       }
     ]
+
+    other_news =
+      case Gettext.get_locale() do
+        "ru" ->
+          [
+            %{
+              id: 4,
+              timestamp: ~U[2022-04-09 12:00:00Z],
+              version: "6.1.2",
+              story: [
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1.0156433551059274,
+                      "value" => "У нас новый апдейт:",
+                      "position" => [11.818352957901183, 144.11386740238515],
+                      "rotation" => -0.000000000000000012232681543189033,
+                      "alignment" => 0,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 0.9361257037749625,
+                      "value" => "• Ресайз фотографий 👇",
+                      "position" => [11.793239287788808, 201.00106122007756],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{
+                    "zoom" => 1.1315270791341807,
+                    "color" => "#6B4D32",
+                    "s3_key" => "28295fd5-eb78-4e87-97c7-61b857bb5f24",
+                    "position" => [-205.18224344932196, -444.03541915699407],
+                    "rotation" => 0
+                  }
+                },
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1,
+                      "value" => "этичный образ жизни",
+                      "answer" => "этичный образ жизни",
+                      "position" => [58.66666666666666, 364.124],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "slow living",
+                      "answer" => "slow living",
+                      "position" => [114, 481.3333231608073],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "гедонизм",
+                      "answer" => "гедонизм",
+                      "position" => [115.66666666666667, 424.3333282470703],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "оптимистическое",
+                      "answer" => "оптимистическое",
+                      "position" => [76, 537.6666666666667],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "• добавили новую \nкатегорию стикеров — \nмировоззрение 🌎",
+                      "position" => [68.16666666666669, 221.33331807454428],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{"color" => "#5E50FC"}
+                },
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 0.8227475861552165,
+                      "value" => "• новый редактор текста ✍️",
+                      "position" => [23.262899318002084, 141.54110603171702],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 0.8160309784078537,
+                      "value" => "• сетка в редакторе 👇",
+                      "position" => [150.2414146415445, 210.82327200741543],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 1,
+                      "action" => "edit_story",
+                      "value" => "ПОПРОБОВАТЬ",
+                      "position" => [106.33333333333331, 697.5],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{
+                    "zoom" => 1.1458749645399164,
+                    "color" => "#2A261E",
+                    "s3_key" => "b318c953-6fc2-4d21-aeba-53b1c64ff931",
+                    "position" => [-199.11932659698599, -430.91464525091305],
+                    "rotation" => 0
+                  }
+                },
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1,
+                      "answer" => "getsince",
+                      "position" => [119.16666666666667, 418.1666819254557],
+                      "question" => "telegram",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "Ждём вашего фидбэка \nздесь 👇",
+                      "position" => [68.66667175292969, 324.6666615804037],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{"color" => "#F97EB9"}
+                }
+              ]
+            }
+          ]
+
+        _ ->
+          [
+            %{
+              id: 4,
+              timestamp: ~U[2022-04-09 12:00:00Z],
+              version: "6.1.2",
+              story: [
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1,
+                      "value" => "• resize photos 👇",
+                      "position" => [11.833333333333314, 195.8333282470703],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 1.0877015758127484,
+                      "value" => "It’s a new update!",
+                      "position" => [11.999989827473968, 133.8963513879968],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{
+                    "zoom" => 1.134265138646841,
+                    "color" => "#6B4D32",
+                    "s3_key" => "a3985666-6cd1-4bf5-8929-ec33f71fc299",
+                    "position" => [-157.090212216804, -339.9593310538014],
+                    "rotation" => 0
+                  }
+                },
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1,
+                      "value" => "• added new category of \nstickers: worldview 🌎",
+                      "position" => [61.66666666666663, 239.33333333333334],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "slow living",
+                      "answer" => "slow living",
+                      "position" => [114, 347.3333282470703],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "optimistic",
+                      "answer" => "optimistic",
+                      "position" => [115, 407.6666717529297],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "ethical lifestyle",
+                      "answer" => "ethical lifestyle",
+                      "position" => [90.5, 465.999994913737],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "hedonism",
+                      "answer" => "hedonism",
+                      "position" => [115.66666666666667, 526.6666666666666],
+                      "question" => "worldview",
+                      "rotation" => 0
+                    }
+                  ],
+                  "background" => %{"color" => "#5E50FC"}
+                },
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1,
+                      "value" => "• new text editor ✍️",
+                      "position" => [20.33334859212256, 129.9573384195963],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "• layout grids 👇",
+                      "position" => [156.6666564941405, 197.83334350585938],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    },
+                    %{
+                      "zoom" => 1,
+                      "action" => "edit_story",
+                      "value" => "TRY",
+                      "position" => [161.0000050862629, 701.1666717529297],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{
+                    "zoom" => 1.187483248072997,
+                    "color" => "#2A261E",
+                    "s3_key" => "b0b1e4eb-b342-4092-a17f-48301784a01b",
+                    "position" => [-219.35540024540654, -474.7075841208285],
+                    "rotation" => 0
+                  }
+                },
+                %{
+                  "size" => [390, 844],
+                  "labels" => [
+                    %{
+                      "zoom" => 1,
+                      "answer" => "getsince",
+                      "position" => [119.16666158040361, 406.6666717529297],
+                      "question" => "telegram",
+                      "rotation" => 0
+                    },
+                    %{
+                      "zoom" => 1,
+                      "value" => "Waiting for your feedback \nhere 👇",
+                      "position" => [57.33332824707023, 310.33333333333337],
+                      "rotation" => 0,
+                      "alignment" => 1,
+                      "text_color" => "#111010",
+                      "corner_radius" => 1,
+                      "background_fill" => "#FFFFFF"
+                    }
+                  ],
+                  "background" => %{"color" => "#F97EB9"}
+                }
+              ]
+            }
+          ]
+      end
+
+    pivot_news ++ other_news
   end
 
   defp last_id do
--- test/t/news/news_test.exs
+++ test/t/news/news_test.exs
@@ -4,25 +4,25 @@ defmodule T.NewsTest do
   alias T.News
 
   @previous_version "6.1.0"
-  @current_version "6.1.1"
+  @current_version "6.1.2"
 
   describe "list_news/1" do
     test "to old user" do
       old_uid = "0000017c-14c7-9745-0242-ac1100020000"
       news = News.list_news(old_uid, @current_version)
-      assert length(news) == 3
+      assert length(news) == 2
     end
 
     test "to not so old user" do
-      not_so_old_uid = "0000017f-b531-3203-0e40-1573d1920000"
+      not_so_old_uid = "0000017f-f572-c244-0aa1-83b884ba0000"
       news = News.list_news(not_so_old_uid, @current_version)
-      assert length(news) == 2
+      assert length(news) == 1
     end
 
     test "to users of previous version" do
       old_uid = "0000017c-14c7-9745-0242-ac1100020000"
       news = News.list_news(old_uid, @previous_version)
-      assert length(news) == 2
+      assert length(news) == 1
     end
   end
 end

fa581fa4557661bd57cb4aa0dc3f70d978c4b8f0
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-09-22 12:30:28 +0400

    update runtime and ci

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule T.MixProject do
     [
       app: :t,
       version: "0.1.6",
-      elixir: "~> 1.13",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),

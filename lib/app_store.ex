84d4e918391835775dcdc921b5dafc93005ebc73
akhrail1996 <akhrail1996@gmail.com>
2022-12-02 21:17:10 +0400

    cont

--- lib/app_store.ex
+++ lib/app_store.ex
@@ -9,7 +9,7 @@ defmodule AppStore do
 
   @type env :: :dev | :prod
   @type notification :: Map.t()
-  @type response :: :ok | {:error, error_reason | Exception.t()}
+  @type response :: :ok | :error
 
   @significant_notification_types [
     "DID_FAIL_TO_RENEW",
@@ -39,8 +39,8 @@ defmodule AppStore do
 
   defp url(:prod), do: "https://api.storekit.itunes.apple.com/inApps/v1/notifications/test/"
 
-  @spec load_notification_history() :: response()
-  def load_notification_history() do
+  @spec load_notification_history(String.t() | nil) :: response()
+  def load_notification_history(pagination_token \\ nil) do
     start_date =
       Notification
       |> order_by([n], desc: n.signed_date)
@@ -54,7 +54,7 @@ defmodule AppStore do
 
     payload = %{"startDate" => start_date, "endDate" => :os.system_time(:millisecond)}
 
-    push(payload, T.Finch)
+    push(payload, pagination_token, T.Finch)
     |> case do
       {:ok, %Finch.Response{status: 200, body: body}} ->
         notifications =
@@ -65,23 +65,58 @@ defmodule AppStore do
 
         process_notifications(notifications)
 
-        # TODO
-        has_more =
-          body
-          |> Jason.decode!()
-          |> Map.fetch!("hasMore")
+        has_more = body |> Jason.decode!() |> Map.fetch!("hasMore")
 
-        IO.inspect("hasMore #{has_more}")
+        if has_more do
+          pagination_token = body |> Jason.decode!() |> Map.fetch!("paginationToken")
+          load_notification_history(pagination_token)
+        else
+          :ok
+        end
 
-        :ok
+      {:ok, response} ->
+        Sentry.capture_message("failed to load notifications from App Store", extra: response)
+        Logger.warn(response)
+        :error
 
-      {:ok, %Finch.Response{body: body}} ->
-        {:error, body |> Jason.decode!() |> error_reason()}
-
-      {:error, _reason} = error ->
+      {:error, reason} = error ->
+        Sentry.capture_message("failed to load notifications from App Store", extra: reason)
         Logger.warn(error)
-        error
+        :error
     end
+  end
+
+  @spec push(notification, String.t() | nil, Finch.name()) ::
+          {:ok, Finch.Response.t()} | {:error, Exception.t()}
+  defp push(notification, pagination_token, finch_name) do
+    notification
+    |> build_request(pagination_token)
+    |> Finch.request(finch_name)
+  end
+
+  @spec build_request(notification, String.t() | nil) :: Finch.Request.t()
+  defp build_request(notification, pagination_token) do
+    {env, token} = AppStore.Token.current_env_and_token()
+    build_request(notification, env, token, pagination_token)
+  end
+
+  @spec build_request(notification, env(), String.t(), String.t() | nil) :: Finch.Request.t()
+  defp build_request(notification, env, token, pagination_token) do
+    url =
+      if pagination_token do
+        url(env) <> "?paginationToken=#{pagination_token}"
+      else
+        url(env)
+      end
+
+    headers = [
+      {"authorization", "Bearer " <> token},
+      {"content-type", "application/json"}
+    ]
+
+    body = Jason.encode!(notification)
+    request = Finch.build(:post, url, headers, body)
+    request
   end
 
   defp process_notifications(notifications),
@@ -215,75 +250,4 @@ defmodule AppStore do
   end
 
   defp maybe_update_premium_status(%Notification{}), do: nil
-
-  @spec push(notification, Finch.name()) :: {:ok, Finch.Response.t()} | {:error, Exception.t()}
-  defp push(notification, finch_name) do
-    notification
-    |> build_request()
-    |> Finch.request(finch_name)
-  end
-
-  @spec build_request(notification) :: Finch.Request.t()
-  defp build_request(notification) do
-    {env, token} = AppStore.Token.current_env_and_token()
-    build_request(notification, env, token)
-  end
-
-  @spec build_request(notification, env(), String.t()) :: Finch.Request.t()
-  defp build_request(notification, env, token) do
-    url = url(env)
-
-    headers = [
-      {"authorization", "Bearer " <> token},
-      {"content-type", "application/json"}
-    ]
-
-    body = Jason.encode!(notification)
-    request = Finch.build(:post, url, headers, body)
-    request
-  end
-
-  defp error_reason(%{"reason" => reason}) do
-    error_reason(reason)
-  end
-
-  error_mappings = %{
-    "BadCollapseId" => :bad_collapse_id,
-    "BadDeviceToken" => :bad_device_token,
-    "BadExpirationDate" => :bad_expiration_date,
-    "BadMessageId" => :bad_message_id,
-    "BadPriority" => :bad_priority,
-    "BadTopic" => :bad_topic,
-    "DeviceTokenNotForTopic" => :device_token_not_for_topic,
-    "DuplicateHeaders" => :duplicate_headers,
-    "IdleTimeout" => :idle_timeout,
-    "InvalidPushType" => :invalid_push_type,
-    "MissingDeviceToken" => :missing_device_token,
-    "MissingTopic" => :missing_topic,
-    "PayloadEmpty" => :payload_empty,
-    "TopicDisallowed" => :topic_disallowed,
-    "BadCertificate" => :bad_certificate,
-    "BadCertificateEnvironment" => :bad_certificate_environment,
-    "ExpiredProviderToken" => :expired_provider_token,
-    "Forbidden" => :forbidden,
-    "InvalidProviderToken" => :invalid_provider_token,
-    "MissingProviderToken" => :missing_provider_token,
-    "BadPath" => :bad_path,
-    "MethodNotAllowed" => :method_not_allowed,
-    "Unregistered" => :unregistered,
-    "PayloadTooLarge" => :payload_too_large,
-    "TooManyProviderTokenUpdates" => :too_many_provider_token_updates,
-    "TooManyRequests" => :too_many_requests,
-    "InternalServerError" => :internal_server_error,
-    "ServiceUnavailable" => :service_unavailable,
-    "Shutdown" => :shutdown
-  }
-
-  error_reasons = Enum.map(error_mappings, fn {_, v} -> v end)
-  error_reason_union = Enum.reduce(error_reasons, &{:|, [], [&1, &2]})
-  @type error_reason :: unquote(error_reason_union)
-
-  for {k, v} <- error_mappings do
-    defp error_reason(unquote(k)), do: unquote(v)
-  end
 end

18274b51fffc8876680d0711cefe927899aab63a
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-05-25 19:18:57 +0800

    fix tailwind config

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,3 +1,8 @@
+// See the Tailwind configuration guide for advanced usage
+// https://tailwindcss.com/docs/configuration
+
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
   theme: {

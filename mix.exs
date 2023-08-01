23276073cc998d477696fd9c2600ff97a35a5315
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-08-01 19:38:46 +0700

    force elixir ~> 1.15

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule T.MixProject do
     [
       app: :t,
       version: "0.1.6",
-      elixir: "~> 1.14",
+      elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),

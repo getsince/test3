0c63a4e9d31ba1072d5d3b814da995a3306d9c40
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-01-06 15:37:05 +0900

    update elixir/erlang/alpine

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule T.MixProject do
     [
       app: :t,
       version: "0.1.6",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
